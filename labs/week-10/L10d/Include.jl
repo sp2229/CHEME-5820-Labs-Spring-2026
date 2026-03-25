c173dffb24a67471e22976ef29777c6237df1479
Jeffrey D Varner <jdv27@cornell.edu>
2026-03-25 12:36:44 -0400

    Lab 10d

--- labs/week-10/L10d/Include.jl
+++ labs/week-10/L10d/Include.jl
@@ -6,6 +6,12 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src");
 # check: do we have a data directory?
 !isdir(_PATH_TO_DATA) && mkpath(_PATH_TO_DATA);
 
+# check do we have a Manifest.toml file?
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
 # load external packages -
 using Statistics
 using LinearAlgebra
@@ -16,3 +22,4 @@ using Flux
 using NNlib
 using Plots
 using Colors
+using JLD2
